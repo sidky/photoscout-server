82250a0f77ec036e7dea38f3844d0ddc4cf13f4d
Manzurur Khan <iamsidky@gmail.com>
2019-03-24 23:44:54 -0700

    fix sql

--- profile/profile.go
+++ profile/profile.go
@@ -14,7 +14,7 @@ type User struct {
 
 func (user User) BookmarkPhoto(photoId string) error {
 	updateStatement := `
-		INSERT INTO starred_photo(user_id, photo_id) VALUES($1, $2) ON CONFLICT 
+		INSERT INTO bookmarks(user_id, photo_id) VALUES($1, $2) ON CONFLICT (user_id, photo_id)
 		DO UPDATE SET updated_at = now() WHERE user_id = $1 AND photo_id = $2
 	`
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
--- sql/definition.sql
+++ sql/definition.sql
@@ -1,6 +1,7 @@
-CREATE TABLE starred_photo (
-  user_id VARCHAR(40) PRIMARY KEY,
+CREATE TABLE bookmarks (
+  user_id VARCHAR(40) NOT NULL,
   photo_id VARCHAR(80) NOT NULL,
   updated_at TIMESTAMP DEFAULT now(),
-  label VARCHAR(30)
+  label VARCHAR(30),
+  PRIMARY KEY (user_id, photo_id)
 )
